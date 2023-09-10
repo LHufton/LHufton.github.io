0a37f03077f88919c9f871a1c736145bac07b1d1
LHufton <lukehufton@gmail.com>
2023-09-10 17:04:04 -0500

    App.jsx .css index.jsx .css

---
+++ src/pages/resume.jsx
@@ -0,0 +1,10 @@
+// Resume.jsx
+import React from 'react'
+
+function Resume() {
+  // Your resume content and JSX code here
+
+  return <div>{/* Resume content */}</div>
+}
+
+export default Resume
